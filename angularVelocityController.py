306eedb48b3c5be4b51c054ad6caf65b98417d86
Anton Evdokimov <a.evdokimov@mts.ai>
2023-02-24 10:26:33 +0300

    add calc angular velocity

--- angularVelocityController.py
+++ angularVelocityController.py
@@ -1,118 +1,128 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-@author: Regislab
-"""
 
 from matplotlib import pyplot as plt
 
 
 class VehicleSimpleDynamic:
-    def __init__(self, mass, k_b, rotorCount, accInit, velInit, posInit):
+    def __init__(self, k_b, l, Iy, accInit, velInit, angleInit):
         '''
-        :param mass: масса аппарата
-        :type mass: float
         :param k_b: коэффициент тяги двигателя
-        :type k_b: float
-        :param rotorCount: количество двигателей в системе
-        :type rotorCount: int
-        :param accInit: начальное значение ускорения ЛА
-        :type accInit: float
-        :param velInit: начальное значение скорости ЛА
-        :type velInit: float
-        :param posInit: начальное значение положения ЛА
-        :type posInit: float
-        '''
-        self.mass = mass
+        :param l: длина базы
+        :param Iy: момент инерции
+       '''
         self.k_b = k_b
-        self.rotorCount = rotorCount
-        self.acceleration = accInit
-        self.velocity = velInit
-        self.position = posInit
-        # Величина ускорения свободного падения
-        self.g = 9.81 
+        self.l = l
+        self.Iy = Iy
+        self.angle_acceleration = accInit
+        self.angle_velocity = velInit
+        self.angle = angleInit
 
-    def rightParts(self, rotorsAngularVel): #rotorsAngularVel: float угловая скорость двигателей
-        rotorsAngularVelSum = 0
-        for i in range(self.rotorCount): # суммируем квадрат заданной угловой скорости для всех двигателей
-            rotorsAngularVelSum += rotorsAngularVel**2
-        # Вычисляем ускорение нашей системы. Получаем силу тяги создаваемую всеми двигателями 
-        # согласно нашей математической модели Fтяги = Kb * omega^2
-        # После для получения текущего ускорения разделим полученное значение 
-        # на массу и вычтем ускорение свободного падения действующие на аппарат
-        self.acceleration = (
-            self.k_b * rotorsAngularVelSum) / self.mass - self.g
+    # rotorsAngularVel: float угловая скорость двигателей
+    def rightParts(self, Acmd):
 
-    def integrate(self, dt): # dt: шаг моделирования
+        # угловые скорости вращения винтов
+        omega1 = Acmd + Tcmd
+        omega2 = -Acmd + Tcmd
+
+        My = self.k_b * self.l * (omega1**2 - omega2**2)
+
+        self.angle_acceleration = My / Iy
+
+    def integrate(self, dt):  # dt: шаг моделирования
         # интегрируем ускорение методом эйлера
-        self.velocity += self.acceleration * dt
+        self.angle_velocity += self.angle_acceleration * dt
         # Полученную скорость интегрируем для определения местоположения
-        self.position += self.velocity * dt
+        self.angle += self.angle_velocity * dt
 
-    def calculatePosition(self, u, dt):
-        '''
-        :param u: управляющие воздействие
-        :type u: float
-        :param dt: шаг моделирования
-        :type dt: float
-        '''
+    def calculateAngle(self, Acmd, dt):
+
         # Для определения положения вызываем метод для правых частей(то есть наших приращений от перемещения)
         # В данном случае приращением выступает ускорение нашей системы.
         # Для следования заданному целевому значению высоты передаем в метод наше управляющие воздействие u,
         # которое характеризует необходимую угловую скорость двигателей.
-        self.rightParts(u)
+        self.rightParts(Acmd)
         # Далее вызываем метод интегрирования
         # Интегрируем полученное при помощи функции правых частей ускорение и получаем скорость,
         # после интегрируем скорость и получаем положение.
         self.integrate(dt)
 
-    def getPosition(self):
-        return self.position  # положение ЛА
+    def getAngle(self):
+        return self.angle  # положение ЛА
 
-    def getVelocity(self):
-        return self.velocity #скорость ЛА
+    def getAngleVelocity(self):
+        return self.angle_velocity  # скорость ЛА
 
-    def getAcceleration(self):
-        return self.acceleration # ускорение
+    def getAngleAcceleration(self):
+        return self.angle_acceleration  # ускорение
 
 
 class ControlSystem():
-    def __init__(self, k_p, k_i, k_d, controlLimit):
+    def __init__(self, k_p, k_i, k_d, k_p_v, k_i_v, k_d_v, velocityControlLimit, accelerationControlLimit):
         self.k_p = k_p
         self.k_i = k_i
         self.k_d = k_d
-        self.desiredPosition = 0
+
+        self.k_p_v = k_p_v
+        self.k_i_v = k_i_v
+        self.k_d_v = k_d_v
+
         self.error = 0
-        self.errorPast = 0
+        self.pastError = 0
+
+        self.desiredAngle = 0
+        self.desiredVelocity = 0
+
+        self.velocityError = 0
+        self.velocityPastError = 0
         self.integral = 0
-        self.controlLimit = controlLimit
+        self.velocityIntegral = 0
+        self.velocityСontrolLimit = velocityControlLimit
+        self.accelerationСontrolLimit = accelerationControlLimit
 
-    def setDesiredPosition(self, desiredPosition):
-        # данный метод устанавливает целевое положение ЛА,
-        # к которому система с течением времени будет стремиться(в нашем примере это высота)
-        self.desiredPosition = desiredPosition  # целевое положение ЛА
+    def setDesiredAngle(self, desiredAngle):
+        # устанавливка целевого угла тангажа ЛА, к которому система с течением времени будет стремиться
+        self.desiredAngle = desiredAngle
 
-    def PID(self, currentPosition, dt):
+    def setDesiredVelocity(self, desiredVelocity):
+        # устанавливка целевого угла тангажа ЛА, к которому система с течением времени будет стремиться
+        self.desiredVelocity = desiredVelocity
+
+    def angle_PID(self, currentAngle, dt):
         '''
-        :param currentPosition: текущее положение ЛА
-        :type currentPosition: float
+        :param currentAngle: текущее угол тангажа ЛА
         :param dt: шаг моделирования
-        :type dt: float
         '''
         # Вычислим функцию ошибки
-        self.error = self.desiredPosition - currentPosition
+        self.error = self.desiredAngle - currentAngle
         # Вычисляем интеграл ошибки
         self.integral += self.error * dt
         # Получим рассчетную управляющую угловую скорость двигателей при помощи ПИД регулятора
         u = self.k_p * self.error + self.k_i * self.integral + \
-            self.k_d * ((self.error - self.errorPast) / dt)
+            self.k_d * ((self.error - self.pastError) / dt)
         # Установим предыдущую ошибку для использования в дальнейших итерациях
-        self.errorPast = self.error
+        self.pastError = self.error
         # Вызовем звено насыщения для ограничения максимального управляющего воздействия
-        u = self.saturation(u)
-        return u
+        u = self.saturation(u, self.velocityСontrolLimit)
+        self.setDesiredVelocity(u)
 
-    def saturation(self, inputVal):
+        return u  # целевая угловая скроксть
+
+    def velocity_PID(self, currentVelocity, dt):
+        self.velocityError = self.desiredVelocity - currentVelocity
+
+        self.velocityIntegral += self.error * dt
+
+        Acmd = self.k_p_v * self.velocityError + self.k_i_v * self.velocityIntegral + \
+            self.k_d_v * ((self.velocityError - self.velocityPastError) / dt)
+        # Установим предыдущую ошибку для использования в дальнейших итерациях
+        self.velocityPastError = self.velocityError
+        # Вызовем звено насыщения для ограничения максимального управляющего воздействия
+        Acmd = self.saturation(Acmd, self.accelerationСontrolLimit)
+
+        return Acmd
+
+    def saturation(self, inputVal, controlLimit):
         '''
         :param inputVal: входное значение
         :type inputVal: float
@@ -120,12 +130,12 @@ class ControlSystem():
         :rtype: float
         '''
         # Звено насыщения ограничивает размер входного параметра
-        # На выходе метода,абсолютное значение не может быть больше 
+        # На выходе метода,абсолютное значение не может быть больше
         # заданного предела controlLimit
-        if inputVal > self.controlLimit:
-            inputVal = self.controlLimit
-        elif inputVal < -self.controlLimit:
-            inputVal = - self.controlLimit
+        if inputVal > controlLimit:
+            inputVal = controlLimit
+        elif inputVal < -controlLimit:
+            inputVal = - controlLimit
 
         return inputVal
 
@@ -147,38 +157,32 @@ class Simulator():
         self.Tend = Tend
         self.controlSys = controlSys
         self.dynamicModel = dynamicModel
-        self.accList = []
-        self.velList = []
-        self.posList = []
+        self.angleAccList = []
+        self.angleVelList = []
+        self.angleList = []
         self.timeList = []
 
-    def runSimulation(self):
-        '''
-       метод запускает моделирование системы от 0 до конечного времени Tend 
-       с шагом dt
-        '''
+    def runSimulation(self):  # моделирование системы от 0 до конечного времени Tend с шагом dt
         # Задаем 0 время и начинаем рассчет до тех пор пока время не достигнет конечного значения Tend
         time = 0
         while (time <= self.Tend):
-            # получаем положение ЛА
-            pose = self.dynamicModel.getPosition()
-            # Получаем скорость ЛА
-            vel = self.dynamicModel.getVelocity()
-            # Получаем ускорение ЛА
-            acc = self.dynamicModel.getAcceleration()
-            # Записываем полученные значения в списки
-            # для дальнейшего построения графиков
-            self.posList.append(pose)
-            self.velList.append(vel)
-            self.accList.append(acc)
+            # получаем угол тангаж ЛА
+            angle = self.dynamicModel.getAngle()
+            # Получаем угловую скорость ЛА
+            velocity = self.dynamicModel.getAngleVelocity()
+            # Получаем угловое ускорение ЛА
+            acc = self.dynamicModel.getAngleAcceleration()
+            # Записываем полученные значения в списки для построения графиков
+            self.angleList.append(angle)
+            self.angleVelList.append(velocity)
+            self.angleAccList.append(acc)
             # self.timeList.append(time)
 
-            # рассчитываем новое управляющие воздействие
-            # на основе текущего положения(pose) ЛА 
-            u = self.controlSys.PID(pose, self.dt)
-            # Рассчитываем положение ЛА с учетом полученного
-            # управляющего воздействия 
-            self.dynamicModel.calculatePosition(u, self.dt)
+            # рассчитываем новое управляющие воздействие на основе текущего угла ЛА
+            u = self.controlSys.angle_PID(angle, self.dt)
+            Acmd = self.controlSys.velocity_PID(velocity, self.dt)
+
+            self.dynamicModel.calculateAngle(Acmd, self.dt)
             # увеличиваем время на dt, то есть на шаг моделирования
             time += self.dt
 
@@ -186,17 +190,17 @@ class Simulator():
         f = plt.figure(constrained_layout=True)
         gs = f.add_gridspec(3, 5)
         ax1 = f.add_subplot(gs[0, :-1])
-        ax1.plot(self.posList)
+        ax1.plot(self.angleList)
         ax1.grid()
-        ax1.set_title('position')
+        ax1.set_title('angle')
 
         ax2 = f.add_subplot(gs[1, :-1])
-        ax2.plot(self.velList, "g")
+        ax2.plot(self.angleVelList, "g")
         ax2.grid()
         ax2.set_title('velocity')
 
         ax3 = f.add_subplot(gs[2, :-1])
-        ax3.plot(self.accList, "r")
+        ax3.plot(self.angleAccList, "r")
         ax3.grid()
         ax3.set_title('acceleration')
 
@@ -205,31 +209,40 @@ class Simulator():
 '''
  Объявим параметры для моделирования
 '''
-k_p = 300 #коэффициент Пропорционального регулирования
-k_i = 35 #коэффициент Интегрального регулирования
-k_d = 180 #коэффициент Дифференциального регулирования
-dt = 0.01 # шаг моделирования системы (например одна сотая секунды)
+k_p = 300  # коэффициент Пропорционального регулирования
+k_i = 35  # коэффициент Интегрального регулирования
+k_d = 180  # коэффициент Дифференциального регулирования
 
-Tend = 20 # конечное время моделирования (например 20 сек)
+k_p_v = 300  # коэффициент Пропорционального регулирования для контура по скорости
+k_i_v = 35  # коэффициент Интегрального регулирования для контура по скорости
+k_d_v = 180  # коэффициент Дифференциального регулирования для контура по скорости
 
-# Масса ЛА
-mass = 0.006
+dt = 0.01  # шаг моделирования системы, сек
+Tend = 10  # конечное время моделирования, сек
+Tcmd = 10  # требуемая тяга
+
 # Коэффициент тяги двигателя ЛА
 k_b = 3.9865e-08
-# Количество двигателей ЛА
-rotorCount = 4
-# Ограничение на угловую скорость двигателей рад/сек
-motorSpeedLimit = 1000
+# момент инерции
+Iy = 7.16914e-05
+# длина базы
+l = 0.17
+# Ограничение на угловую скорость поворота по углу тангажа, рад/сек
+velocityControlLimit = 1000
+accelerationControlLimit = 1000
 
 '''
 Создадим объект контроллера и объект для нашей математической модели
 '''
-controller = ControlSystem(k_p, k_i, k_d, motorSpeedLimit)
-uavSimpleDynamic = VehicleSimpleDynamic(mass, k_b, rotorCount, 0, 0, 0)
+controller = ControlSystem(
+    k_p, k_i, k_d, k_p_v, k_i_v, k_d_v,  velocityControlLimit, accelerationControlLimit)
+
+uavSimpleDynamic = VehicleSimpleDynamic(k_b, l, Iy, 0, 0, 0)
 '''
-Установим целевое положение для нашей системы
+Установим целевой угол тангажа для нашей системы
 '''
-controller.setDesiredPosition(10)
+desiredAngle = 30
+controller.setDesiredAngle(desiredAngle)
 
 """
 Создадим объект симулятора и передадим в него контроллер
